82d69c80640bf0df782316cb91d1fb746c75ac07
yasokada <yasokada@gmail.com>
2018-03-10 09:07:05 +0900

    use default theme

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to ReadTheDocs_180310\u2019s documentation!"],titleterms:{document:0,indic:0,readthedocs_180310:0,tabl:0,welcom:0}})
